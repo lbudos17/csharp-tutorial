fe7ee7e4bea3d38e60f0619c7654e5cb35d6b469
lbudos17 <leona.budos@gmail.com>
2023-08-10 17:40:58 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string hello = "Dobar dan";
+string poruka = "Ovo je poruka";
+Console.WriteLine(hello);
+Console.WriteLine(poruka);
+
+// tip nazivVarijable = vrijednost;
+// int number = 120;
+// double decimalNum = 3.145;
+// string valuta = $;
+// Boolean trueafalse = false;
+
+string number = "120";
+string valuta = "180";
+
+
+string result = ((int.Parse(number) + int.Parse(valuta)) / 100).ToString();
+
+Console.WriteLine("Cijena proizvoda je: " + result);
